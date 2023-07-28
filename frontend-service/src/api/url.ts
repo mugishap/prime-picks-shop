c4952411c600bf646b165a2d85108bd236b44502
Mugisha Precieux <precieuxmugisha@gmail.com>
2023-07-28 09:30:55 +0200

    [fix]: Update to render

--- frontend-service/src/api/url.ts
+++ frontend-service/src/api/url.ts
@@ -1,6 +1,6 @@
 const apiUrl: string = import.meta.env.DEV
     ? "http://localhost:5000/api/v1"
-    : "https://primepicks.up.railway.app/api/v1";
+    : "https://https://primepicks.onrender.com/api/v1";
 
 export {
     apiUrl
